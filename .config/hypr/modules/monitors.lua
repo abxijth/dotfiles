1419123314503e672ddaf769b4bb3bfa1cc12c7c
abxijth <abhijithrpillai231@gmail.com>
2026-05-29 20:53:04 +0530

    add initial dotfiles configuration

---
+++ .config/hypr/modules/monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+  output = "HDMI-A-2",
+  mode = "1920x1080@74.97",
+  position = "0x0",
+  scale = 1,
+})
